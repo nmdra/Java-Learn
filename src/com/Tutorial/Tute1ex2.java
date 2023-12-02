30e0246409c704fa42e5fb6d6bd6d7cd3ce6afd8
nmdra <nimendradilshan11@gmail.com>
2023-12-02 21:29:05 +0530

    Update Tutorial

--- src/com/Tutorial/Tute1ex2.java
+++ src/com/Tutorial/Tute1ex2.java
@@ -6,7 +6,7 @@ package Tutorial;
 import java.util.Scanner;
 
 class Tute1ex2 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         Scanner getNum = new Scanner(System.in);
         int number, count = 0;
--- src/com/Tutorial/Tute1ex6.java
+++ src/com/Tutorial/Tute1ex6.java
@@ -1,2 +1,44 @@
-package Tutorial;public class Tute1ex6 {
+package Tutorial;
+
+import java.util.Scanner;
+
+public class Tute1ex6 {
+    public static void main(String[] args) {
+
+        Scanner getStr = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str = getStr.next();
+
+        System.out.print("Enter String: ");
+        char character = getStr.next().charAt(0);
+
+        System.out.println("Character \"" + character +
+                "\" Occurrence(using loop): " + countChar(str, character));
+        System.out.println("Character \"" + character +
+                "\" Occurrence(recursion): " + countCharRec(str, character, 0));
+    }
+
+    // Count character Occurrence using Loop
+    static int countChar (String string, char a) {
+        int count = 0;
+        for(int i = 0; i < string.length(); i++) {
+            count += (string.charAt(i) == a) ? 1 : 0;
+        }
+
+        return count;
+    }
+
+    // Count Character Occurrence using recursion
+    static int countCharRec (String string, char a, int index) {
+
+        if (index >= string.length()){
+            return 0;
+        }
+
+        int count = string.charAt(index) == a ? 1 : 0;
+
+        return count + (countCharRec(string, a, index + 1));
+
+    }
 }
