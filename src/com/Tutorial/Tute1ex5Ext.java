e3362c0703dd739e2b1e79140576f021a9b563c6
nmdra <nimendradilshan11@gmail.com>
2023-12-02 22:28:45 +0530

    Update exercise 5 with recursion

--- src/com/Tutorial/Tute1ex5Ext.java
+++ src/com/Tutorial/Tute1ex5Ext.java
@@ -1,8 +1,10 @@
+// This program counts the occurrences of a specific character (e.g., 'a') in a given string case-insensitively:
+
 package Tutorial;
 
 import java.util.Scanner;
 
-public class Tute1ex6 {
+public class Tute1ex5Ext {
     public static void main(String[] args) {
 
         Scanner getStr = new Scanner(System.in);
@@ -10,20 +12,22 @@ public class Tute1ex6 {
         System.out.print("Enter String: ");
         String str = getStr.next();
 
-        System.out.print("Enter String: ");
+        System.out.print("Enter Character: ");
         char character = getStr.next().charAt(0);
 
-        System.out.println("Character \"" + character +
-                "\" Occurrence(using loop): " + countChar(str, character));
-        System.out.println("Character \"" + character +
-                "\" Occurrence(recursion): " + countCharRec(str, character, 0));
+        character = checkCase(character) ? Character.toLowerCase(character) : character;
+
+        System.out.println("Character " +
+                " Occurrence(using loop): " + countChar(str, character));
+        System.out.println("Character " +
+                " Occurrence(recursion): " + countCharRec(str, character, 0));
     }
 
     // Count character Occurrence using Loop
     static int countChar (String string, char a) {
         int count = 0;
         for(int i = 0; i < string.length(); i++) {
-            count += (string.charAt(i) == a) ? 1 : 0;
+            count += (string.toLowerCase().charAt(i) == a) ? 1 : 0;
         }
 
         return count;
@@ -36,9 +40,13 @@ public class Tute1ex6 {
             return 0;
         }
 
-        int count = string.charAt(index) == a ? 1 : 0;
+        int count = string.toLowerCase().charAt(index) == a ? 1 : 0;
 
         return count + (countCharRec(string, a, index + 1));
 
+    }
+
+    static boolean checkCase (char ch) {
+        return Character.isUpperCase(ch);
     }
 }
