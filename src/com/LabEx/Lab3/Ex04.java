8e1226e266bc09c3a31e1bfa8d4bf0f931a4e2c1
nmdra <nimendradilshan11@gmail.com>
2024-03-01 16:29:36 +0530

    Update

--- src/com/LabEx/Lab3/Ex04.java
+++ src/com/LabEx/Lab3/Ex04.java
@@ -5,24 +5,9 @@ class Student{
     String ditno;
     String address;
 
-    public Student() {
-    }
-
     public Student(String name, String ditno, String address) {
         this.name = name;
         this.ditno = ditno;
-        this.address = address;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDitno(String ditno) {
-        this.ditno = ditno;
-    }
-
-    public void setAddress(String address) {
         this.address = address;
     }
 
@@ -32,16 +17,28 @@ class Student{
         System.out.println("Address: " + address);
     }
 
+    void setName(String name){
+        this.name = name;
+    }
+
+    void setAddress(String address){
+        this.address = address;
+    }
+
+    void setDit(String ditno){
+        this.ditno = ditno;
+    }
+
     String getName(){
-        return name;
+        return this.name;
     }
 
     String getAddress(){
-        return address;
+        return this.address;
     }
 
     String getDit(){
-        return ditno;
+        return this.ditno;
     }
 
     String getDetails(){
@@ -51,16 +48,15 @@ class Student{
 
 public class Ex04 {
     public static void main(String[] args) {
-        Student student1 = new Student("Name", "123", "123 Main St");
-        Student student2 = new Student();
 
-        student2.setDitno("234");
-        student2.setName("Test");
-        student2.setAddress("Colombo");
+        Student student1 = new Student("Kaveen", "123", "123 Main St");
+        Student student2 = new Student("Dinethma", "452", "456 Flower St");
+
+//        student1.setAddress("422 Flower road");
+//        student2.setName("Kaveen");
+//        student2.setDit("3242");
 
         System.out.println(student1.getName());
-        System.out.println(student1.getAddress());
-        System.out.println(student1.getDit());
 
         System.out.println(student1.getDetails());
     }
