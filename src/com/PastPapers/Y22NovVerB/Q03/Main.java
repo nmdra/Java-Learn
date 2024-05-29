f7a6638a240b8934ceccf6f2fb87ed3d3010782f
nmdra <nimendradilshan11@gmail.com>
2024-05-29 08:28:17 +0530

    2019 Thread Question

---
+++ src/com/PastPapers/Y22NovVerB/Q03/Main.java
@@ -0,0 +1,29 @@
+package PastPapers.Y22NovVerB.Q03;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Object lock = new Object();
+		String pattern1, pattern2;
+		int count = 10;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter Pattern 1 = ");
+		pattern1 = sc.nextLine();
+		
+		System.out.print("Enter Pattern 2 = ");
+		pattern2= sc.nextLine();
+				
+		System.out.print("Enter count = ");
+		sc.nextInt();
+
+		System.out.println();
+
+		Thread th2 = new Thread(new Pattern02(lock, pattern2, count));
+		th2.start();
+		Thread th = new Thread(new Pattern01(lock, pattern1, count));
+		th.start();
+	}
+}
---
+++ src/com/PastPapers/Y22NovVerB/Q03/Pattern02.java
@@ -0,0 +1,41 @@
+package PastPapers.Y22NovVerB.Q03;
+
+public class Pattern02 implements Runnable {
+	Object lock;
+	String pattern;
+	int count;
+
+	public Pattern02(Object lock, String pattern, int count) {
+		super();
+		this.lock = lock;
+		this.pattern = pattern;
+		this.count = count;
+	}
+
+	@Override
+	public void run() {
+		synchronized (lock) {
+
+			for(int i = 0; i < count; i++) {
+				System.out.print("Pattern 02 Thread =  ");
+				for(int j = 0; j < i; j++) {
+					System.out.print(" ");
+				}
+				for(int k = 0; k < (count - i); k++ ) {
+					System.out.print(pattern + " ");
+				}
+
+				System.out.println();
+				try {
+					Thread.sleep(1000);
+					lock.notify();
+					lock.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			lock.notifyAll();
+		}
+	}
+}
